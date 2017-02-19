dcc1a644286877ec80fc969383c6eaa897ecde07
myc1995 <875940302@qq.com>
2017-02-19 16:52:52 +0800

    update entity

---
+++ src/com/rupeng/bookstore/entity/Address.java
@@ -0,0 +1,115 @@
+package com.rupeng.bookstore.entity;
+
+import java.util.Date;
+
+public class Address
+{
+    private Integer id;
+    private int userId;
+    private String consigee;// 收货人
+    private String phone;
+    private String location;
+    private Date datetime;
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Address other = (Address) obj;
+        if (id == null)
+        {
+            if (other.id != null)
+                return false;
+        }
+        else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public int getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(int userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getConsigee()
+    {
+        return consigee;
+    }
+
+    public void setConsigee(String consigee)
+    {
+        this.consigee = consigee;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+
+    public Date getDatetime()
+    {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime)
+    {
+        this.datetime = datetime;
+    }
+
+    public boolean isDefault()
+    {
+        return isDefault;
+    }
+
+    public void setDefault(boolean isDefault)
+    {
+        this.isDefault = isDefault;
+    }
+
+    private boolean isDefault;
+
+}
